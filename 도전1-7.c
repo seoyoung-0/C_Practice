1d22434c800d2c4cea7578d351965b3766969c89
seoyoung-0 <kdy03013@gmail.com>
2019-07-08 19:03:20 +0900

    n보다 작거나 같기위위한 2의 k승수 구하기

---
+++ 도전1-7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+	int n, k;
+	int result = 2;
+
+	printf(" n :");
+	scanf_s("%d", &n);
+
+	k = 0;
+	while (result <= n) {
+		result = result * 2;
+		k++;
+	}
+	printf("k의 최대값; %d", k);
+}
